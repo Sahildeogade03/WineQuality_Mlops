d30b813c54a1f48a68859b36cc7411bc18dbcadf
Sahildeogade03 <130857121+Sahildeogade03@users.noreply.github.com>
2026-09-16 17:59:01 +0000

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WineQuality_Mlops",
+    version="0.1.0",
+    description="A machine learning project for predicting wine quality",
+    author="Sahil Deogade",
+    packages=find_packages(),
+    license="MIT"
+)
---
+++ tests/test_config.py
@@ -0,0 +1,4 @@
+def test_generic():
+    a = 5
+    b = 5
+    assert a == b
